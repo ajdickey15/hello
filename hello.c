3438ab5f1bd8283fde13ed28ef848e5af60983f2
Austin Dickey <adickey4@huskers.unl.edu>
2020-08-24 18:47:54 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Austin Dickey
+ * Date: 2020/08/17
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Austin Dickey\n");
+  printf("Computer Science\n");
+
+  return 0;
+}
